7c852b22fd0c4fdf06ab9767319e8bc1d8d5ae29
uday12381 <udaykumar12381@gmail.com>
2024-02-04 20:36:31 +0530

    Fixed homepage path

--- jlnrco/src/components/home.jsx
+++ jlnrco/src/components/home.jsx
@@ -12,11 +12,29 @@ import CALogo from "../assets/ca_logo.jpeg";
 import { NavigationBar } from "./navigation-bar";
 import { OurServices } from "./our-services";
 import { Footer } from "./footer";
+import { Banner, KIND } from "baseui/banner";
+
+// @ts-ignore
+import DeleteAlt from "baseui/icon/delete-alt";
+
+function ArtworkIcon({ size }: { size: string }) {
+  return <DeleteAlt size={size} aria-label="icon label" />;
+}
 
 export default function HomePage() {
   return (
     <>
       <NavigationBar />
+      <Banner
+        title="This site is under construction. Apologies for the inconvenience..."
+        kind={KIND.negative}
+        artwork={{
+          icon: ArtworkIcon,
+        }}
+      >
+        Please contact us at{" "}
+        {<a href="mailto:jlnrco@gmail.com">jlnrco@gmail.com</a>}.
+      </Banner>
       <OurServices />
       <Footer />
     </>
